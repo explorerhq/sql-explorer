547f30ccf219902878aee0fc9ae6198c53d7f067
chrisclark <chris@untrod.com>
2013-12-27 14:53:42 -0500

    prepopulating created by user for new reports

--- report/forms.py
+++ report/forms.py
@@ -1,15 +1,9 @@
 from django.forms import ModelForm
 from report.models import Report
-from django import forms
+
 
 class ReportForm(ModelForm):
 
     class Meta:
         model = Report
         fields = ['title', 'sql', 'description', 'created_by']
-        widgets = {
-            'title': forms.TextInput(attrs={'class': 'form-control'}),
-            'sql': forms.Textarea(attrs={'class': 'form-control'}),
-            'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 4}),
-            'created_by': forms.TextInput(attrs={'class': 'form-control'}),
-        }
