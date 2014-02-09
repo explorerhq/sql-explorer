a234b5f1f9c1ebabe0d98c8a2f0828a3576e6bd5
chrisclark <chris@untrod.com>
2014-02-08 18:07:20 -0800

    minor refactor

--- explorer/utils.py
+++ explorer/utils.py
@@ -77,7 +77,7 @@ def schema_info():
             ret.append((
                           friendly_model,
                           model._meta.db_table,
-                          [(f.get_attname_column()[1], f.get_internal_type()) for f in model._meta.fields]
+                          [_format_field(f) for f in model._meta.fields]
                       ))
 
             #Do the same thing for many_to_many fields. These don't show up in the field list of the model
@@ -85,10 +85,15 @@ def schema_info():
             ret += [(
                        friendly_model,
                        m2m.rel.through._meta.db_table,
-                       [(f.get_attname_column()[1], f.get_internal_type()) for f in m2m.rel.through._meta.fields]
+                       [_format_field(f) for f in m2m.rel.through._meta.fields]
                     ) for m2m in model._meta.many_to_many]
 
     return sorted(ret, key=lambda t: t[1])  # sort by table name
+
+
+def _format_field(field):
+    return (field.get_attname_column()[1], field.get_internal_type())
+
 
 
 def param(name):
