3b63cf45ad2de79a4baaa5d608c241e17b6d63b3
chrisclark <chris@untrod.com>
2015-02-16 17:48:45 -0800

    fixing tests

--- explorer/tests/test_models.py
+++ explorer/tests/test_models.py
@@ -9,22 +9,23 @@ class TestQueryModel(TestCase):
     def test_blacklist_check_runs_before_execution(self):
         q = SimpleQueryFactory(sql='select 1;')
         with AssertMethodIsCalled(q, "passes_blacklist"):
-            headers, data, duration, error = q.headers_and_data()
+            res = q.headers_and_data()
 
     def test_blacklist_prevents_bad_sql_from_executing(self):
         q = SimpleQueryFactory(sql='select 1 "delete";')
-        headers, data, duration, error = q.headers_and_data()
-        self.assertEqual(error, MSG_FAILED_BLACKLIST)
+        res = q.headers_and_data()
+        self.assertEqual(res.error, MSG_FAILED_BLACKLIST)
 
     def test_blacklist_prevents_bad_sql_with_params_from_executing(self):
         q = SimpleQueryFactory(sql="select '$$foo$$';")
-        headers, data, duration, error = q.headers_and_data(params={"foo": "'; delete from *; select'"})
-        self.assertEqual(error, MSG_FAILED_BLACKLIST)
+        q.params = {"foo": "'; delete from *; select'"}
+        res = q.headers_and_data()
+        self.assertEqual(res.error, MSG_FAILED_BLACKLIST)
 
     def test_params_get_merged(self):
         q = SimpleQueryFactory(sql="select '$$foo$$';")
-        params = {'foo': 'bar', 'mux': 'qux'}
-        self.assertEqual(q.available_params(params), {'foo': 'bar'})
+        q.params = {'foo': 'bar', 'mux': 'qux'}
+        self.assertEqual(q.available_params(), {'foo': 'bar'})
 
     def test_query_log(self):
         self.assertEqual(0, QueryLog.objects.count())
