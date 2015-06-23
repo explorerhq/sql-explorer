1b12e5bd1069210a037cb3c7a92986a63c820138
Anton-Shutik <anton.shutik@itechart-group.com>
2015-06-23 15:26:46 +0300

    Make "created_by_user" raw_id_field

--- explorer/admin.py
+++ explorer/admin.py
@@ -6,6 +6,7 @@ from explorer.actions import generate_report_action
 class QueryAdmin(admin.ModelAdmin):
     list_display = ('title', 'description', 'created_by_user',)
     list_filter = ('title',)
+    raw_id_fields = ('created_by_user',)
     
     actions = [generate_report_action()]
 
