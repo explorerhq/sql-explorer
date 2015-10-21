9d1b7d86d02d1f57aeb9c5adc11668fee5cc912a
chrisclark <chris@untrod.com>
2015-10-21 11:31:28 -0700

    removing invalid classifier for 0.8.0 release

--- setup.py
+++ setup.py
@@ -30,8 +30,7 @@ setup(
         'Topic :: Utilities',
         'Framework :: Django :: 1.6',
         'Framework :: Django :: 1.7',
-        'Framework :: Django :: 1.8',
-        'Framework :: Django :: 1.9',
+        'Framework :: Django :: 1.8', #       'Framework :: Django :: 1.9', -- not supported on PyPi yet
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
