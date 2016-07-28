fbed46212aef3f9207536552b815fbb8576989c6
Jared Proffitt <jproffitt@worthwhile.com>
2016-07-28 10:22:47 -0500

    Fix #212 - eliminate duplicated tables in schema view

--- explorer/utils.py
+++ explorer/utils.py
@@ -51,21 +51,14 @@ def schema_info():
                 app.name in app_settings.EXPLORER_SCHEMA_EXCLUDE_APPS:
             continue
 
-        for model_name, model in apps.get_app_config(label).models.items():
-            friendly_model = "%s -> %s" % (app.name, model._meta.object_name)
-            ret.append((
-                          friendly_model,
-                          model._meta.db_table,
-                          [_format_field(f) for f in model._meta.fields]
-                      ))
-
-            # Do the same thing for many_to_many fields. These don't show up in the field list of the model
-            # because they are stored as separate "through" relations and have their own tables
-            ret += [(
-                       friendly_model,
-                       m2m.rel.through._meta.db_table,
-                       [_format_field(f) for f in m2m.rel.through._meta.fields]
-                    ) for m2m in model._meta.many_to_many]
+        if app.name not in app_settings.EXPLORER_SCHEMA_EXCLUDE_APPS:
+            for model in apps.get_app_config(label).get_models(include_auto_created=True):
+                friendly_model = "%s -> %s" % (app.name, model._meta.object_name)
+                ret.append((
+                              friendly_model,
+                              model._meta.db_table,
+                              [_format_field(f) for f in model._meta.fields]
+                          ))
 
     return sorted(ret, key=lambda t: t[1])
 
