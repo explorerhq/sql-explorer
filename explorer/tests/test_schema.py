c4c0a189a1574fe4d2e7ea3a4c21d45b6a44526e
chrisclark <chris@untrod.com>
2016-11-22 16:58:09 -0800

    another attempt at py3 tests to pass

--- explorer/tests/test_schema.py
+++ explorer/tests/test_schema.py
@@ -7,12 +7,12 @@ from explorer.schema import schema_info
 class TestSchemaInfo(TestCase):
 
     def setUp(self):
-        self.old_incl = app_settings.EXPLORER_SCHEMA_INCLUDE_TABLE_PREFIXES
-        self.old_excl = app_settings.EXPLORER_SCHEMA_EXCLUDE_TABLE_PREFIXES
+        app_settings.EXPLORER_SCHEMA_INCLUDE_TABLE_PREFIXES = None
+        app_settings.EXPLORER_SCHEMA_EXCLUDE_TABLE_PREFIXES = []
 
     def tearDown(self):
-        app_settings.EXPLORER_SCHEMA_INCLUDE_TABLE_PREFIXES = self.old_incl
-        app_settings.EXPLORER_SCHEMA_EXCLUDE_TABLE_PREFIXES = self.old_excl
+        app_settings.EXPLORER_SCHEMA_INCLUDE_TABLE_PREFIXES = None
+        app_settings.EXPLORER_SCHEMA_EXCLUDE_TABLE_PREFIXES = []
 
     def test_schema_info_returns_valid_data(self):
         res = schema_info(get_connection())
@@ -39,4 +39,3 @@ class TestSchemaInfo(TestCase):
         res = schema_info(get_connection())
         tables = [x[0] for x in res]
         self.assertIn('explorer_query', tables)
-
