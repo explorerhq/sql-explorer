cf40c04978e24562d58720294e30f8f655881188
Chris Clark <chris@untrod.com>
2017-07-03 15:10:29 -0700

    consistent use of patch

--- explorer/tests/test_schema.py
+++ explorer/tests/test_schema.py
@@ -10,36 +10,40 @@ class TestSchemaInfo(TestCase):
     def setUp(self):
         cache.clear()
 
-    def tearDown(self):
-        schema._get_includes = lambda: None
-        schema._get_excludes = lambda: []
-
-    def test_schema_info_returns_valid_data(self):
-        schema._get_includes = lambda: None
-        schema._get_excludes = lambda: []
+    @patch('explorer.schema._get_includes')
+    @patch('explorer.schema._get_excludes')
+    def test_schema_info_returns_valid_data(self, mocked_excludes, mocked_includes):
+        mocked_includes.return_value = None
+        mocked_excludes.return_value = []
         res = schema.schema_info(CONN)
         tables = [x[0] for x in res]
         self.assertIn('explorer_query', tables)
 
-    def test_table_exclusion_list(self):
-        schema._get_includes = lambda: None
-        schema._get_excludes = lambda: ('explorer_',)
+    @patch('explorer.schema._get_includes')
+    @patch('explorer.schema._get_excludes')
+    def test_table_exclusion_list(self, mocked_excludes, mocked_includes):
+        mocked_includes.return_value = None
+        mocked_excludes.return_value = ('explorer_',)
         res = schema.schema_info(CONN)
         tables = [x[0] for x in res]
         self.assertNotIn('explorer_query', tables)
 
-    def test_app_inclusion_list(self, ):
-        schema._get_includes = lambda: ('auth_',)
-        schema._get_excludes = lambda: []
+    @patch('explorer.schema._get_includes')
+    @patch('explorer.schema._get_excludes')
+    def test_app_inclusion_list(self, mocked_excludes, mocked_includes):
+        mocked_includes.return_value = ('auth_',)
+        mocked_excludes.return_value = []
         res = schema.schema_info(CONN)
         tables = [x[0] for x in res]
         self.assertNotIn('explorer_query', tables)
         self.assertIn('auth_user', tables)
 
-    def test_app_inclusion_list_excluded(self):
+    @patch('explorer.schema._get_includes')
+    @patch('explorer.schema._get_excludes')
+    def test_app_inclusion_list_excluded(self, mocked_excludes, mocked_includes):
         # Inclusion list "wins"
-        schema._get_includes = lambda: ('explorer_',)
-        schema._get_excludes = lambda: ('explorer_',)
+        mocked_includes.return_value = ('explorer_',)
+        mocked_excludes.return_value = ('explorer_',)
         res = schema.schema_info(CONN)
         tables = [x[0] for x in res]
         self.assertIn('explorer_query', tables)
