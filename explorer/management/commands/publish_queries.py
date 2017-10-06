13213d83d92c0fa500d8e8c1ca9855af27939572
andres <andres@moni.com.ar>
2017-10-06 13:12:30 -0300

    added publish query as management command

---
+++ explorer/management/commands/publish_queries.py
@@ -0,0 +1,19 @@
+from celery.utils.log import get_task_logger
+from django.core.management.base import BaseCommand
+from explorer.models import Query, FTPExport
+from explorer.tasks import snapshot_query_on_bucket
+
+logger = get_task_logger(__name__)
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        logger.info("Starting query snapshots...")
+        qs_bucket = Query.objects.exclude(bucket__exact='').values_list('id', flat=True)
+        qs_ftp = FTPExport.objects.all().values_list('query__id', flat=True)
+        total_ids = set(list(qs_bucket)+list(qs_ftp))
+        logger.info("Found %s queries to snapshot. Creating snapshot tasks..." % len(total_ids))
+        for qid in total_ids:
+            snapshot_query_on_bucket.delay(qid)
+        logger.info("Done creating tasks.")
