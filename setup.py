c831f6bee344b67171900ca7e50bf6fc85ea9345
Dane Hillard <github@danehillard.com>
2018-08-14 12:13:16 -0400

    Remove outdated qualifier in setup.py

--- setup.py
+++ setup.py
@@ -28,7 +28,6 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Topic :: Utilities',
-        'Framework :: Django :: 1.7',
         'Framework :: Django :: 1.8',
         'Framework :: Django :: 1.9',
         'Framework :: Django :: 1.10',
