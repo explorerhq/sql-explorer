52273368cf488368504392cc0af57074b18844d8
Harm Verhagen <harm@symeon.nl>
2018-08-16 17:13:31 +0200

    Fix  auth.views.login (to support Django2.1)
    django.contrib.auth.views.login was removed from Django 2.1.
    Use django.conrib.auth.login instead.

--- explorer/utils.py
+++ explorer/utils.py
@@ -2,7 +2,10 @@ import functools
 import re
 
 from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth.views import login
+try:
+    from django.contrib.auth import login
+except ImportError:
+    from django.contrib.auth.views import login
 from django.contrib.auth import REDIRECT_FIELD_NAME
 from six import text_type
 import sqlparse
