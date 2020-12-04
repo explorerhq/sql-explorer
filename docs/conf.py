26b54805ec8b7828d4890d6c58e5f7f5db8d8d5e
Mark Walker <mark.walker@realbuzz.com>
2020-12-04 22:20:32 +0000

    Doesn't need the file extension on `master_doc` and RTD isn't clever enough to detect if you've included it...

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ author = 'Grove Co'
 
 # -- General configuration ---------------------------------------------------
 
-master_doc = 'index.rst'
+master_doc = 'index'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
