d89e5a4ff49b1345255435a8a945e43934064b83
Mark Walker <mark.walker@realbuzz.com>
2020-12-04 22:02:15 +0000

    Add config for master doc for sphinx RTD build

--- docs/conf.py
+++ docs/conf.py
@@ -24,6 +24,8 @@ author = 'Grove Co'
 
 # -- General configuration ---------------------------------------------------
 
+master_doc = 'index.rst'
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
