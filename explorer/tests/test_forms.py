0d4ae16d9ef5a89bcb272a6c97496c47dbf0977d
John Vandenberg <jayvdb@gmail.com>
2020-12-19 22:26:57 +0700

    models: Require Query.sql (#425)
    * models: Require Query.sql
    
    * forms: Require Query.sql
    
    * views: Ensure sql is not None

--- explorer/tests/test_forms.py
+++ explorer/tests/test_forms.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from django.db.utils import IntegrityError
 from django.forms.models import model_to_dict
 from django.test import TestCase
 
@@ -12,6 +13,16 @@ class TestFormValidation(TestCase):
         q = SimpleQueryFactory(sql="select 1;", created_by_user_id=None)
         form = QueryForm(model_to_dict(q))
         self.assertTrue(form.is_valid())
+
+    def test_form_fails_null(self):
+        with self.assertRaises(IntegrityError):
+            SimpleQueryFactory(sql=None, created_by_user_id=None)
+
+    def test_form_fails_blank(self):
+        q = SimpleQueryFactory(sql='', created_by_user_id=None)
+        q.params = {}
+        form = QueryForm(model_to_dict(q))
+        self.assertFalse(form.is_valid())
 
     def test_form_fails_blacklist(self):
         q = SimpleQueryFactory(sql="delete $$a$$;", created_by_user_id=None)
