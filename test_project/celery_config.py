bbeef75071812730721a6bb942514256ceb48637
Rick Lawson <lawson89@gmail.com>
2022-12-16 14:09:38 -0500

    Js plugin to export pivot table to excel
    Issue #459

--- test_project/celery_config.py
+++ test_project/celery_config.py
@@ -2,6 +2,7 @@ import os
 
 from celery import Celery
 
+
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'test_project.settings')
 
