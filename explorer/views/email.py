a9e3299cbddd17215bb61441eed5667d34a85330
Mark Walker <mark.walker@realbuzz.com>
2023-01-19 17:55:16 +0000

    ci: ran pre-commit for ci bot integration

--- explorer/views/email.py
+++ explorer/views/email.py
@@ -10,7 +10,7 @@ class EmailCsvQueryView(PermissionRequiredMixin, View):
     permission_required = 'view_permission'
 
     def post(self, request, query_id, *args, **kwargs):
-        if request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest':
+        if request.headers.get('x-requested-with') == 'XMLHttpRequest':
             email = request.POST.get('email', None)
             if email:
                 execute_query.delay(query_id, email)
