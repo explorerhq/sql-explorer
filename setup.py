a2ff810ba75005ca82276ae5fc0038a066911a3c
Mark Walker <mwalker@ismgames.com>
2023-02-25 20:54:49 +0000

    build: 3.1 release prep

--- setup.py
+++ setup.py
@@ -73,6 +73,7 @@ setup(
         'Framework :: Django :: 3.2',
         'Framework :: Django :: 4.0',
         'Framework :: Django :: 4.1',
+        'Framework :: Django :: 4.2',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
