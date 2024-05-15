88b571c63bb7ad0f9ca9b084b40ebdf44e4b5a99
Calum Smith <hello@calum.io>
2024-05-15 15:35:50 -0400

    Fix CSRF when CSRF_USE_SESSIONS is enabled
    The front-end currently checks for the
    `input[name="csrfmiddlewaretoken"]` element if `CSRF_COOKIE_HTTPONLY` is
    set, but according to the [Django docs], it should do the same thing if
    `CSRF_USE_SESSIONS` is set, as it will also mean the token is not
    available in a cookie.
    
    [Django docs]: https://docs.djangoproject.com/en/5.0/howto/csrf/#acquiring-the-token-if-csrf-use-sessions-or-csrf-cookie-httponly-is-true

--- explorer/src/js/csrf.js
+++ explorer/src/js/csrf.js
@@ -1,10 +1,10 @@
 import cookie from "cookiejs";
 
 const csrfCookieName = document.getElementById('csrfCookieName').value;
-const csrfCookieHttpOnly = document.getElementById('csrfCookieHttpOnly').value === "True";
+const csrfTokenInDOM = document.getElementById('csrfTokenInDOM').value === "True";
 
 export function getCsrfToken() {
-    if (csrfCookieHttpOnly) {
+    if (csrfTokenInDOM) {
         let csrfInput = document.querySelector('input[name="csrfmiddlewaretoken"]');
         return csrfInput ? csrfInput.value : null;
     }
